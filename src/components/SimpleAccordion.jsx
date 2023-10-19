cc5e5495be025c8b1054a05ebde408382153e8bf
IGuidoo <iguidoo@outlook.com>
2023-10-19 20:07:22 +0200

    acc fix

--- src/components/SimpleAccordion.jsx
+++ src/components/SimpleAccordion.jsx
@@ -12,8 +12,8 @@ const Accordion = ({ data = [], cssClass, cssIndivClass }) => {
                 <div key={item.title}>
                     <input type="checkbox" name={`detail-${index}`} id={`detail-${index}`} className='simple-accordion__input' />
                     <details open  className={` simple-accordion text-base leading-6 font-medium text-secondary-700 md:text-lg md:leading-7 ${cssIndivClass}`}>
-                        <summary className='p-8'>
-                            <label for={`detail-${index}`} className='flex gap-6 simple-accordion__label'>
+                        <summary className=''>
+                            <label for={`detail-${index}`} className='flex gap-6 simple-accordion__label py-5'>
                                 <h3 className='text-xl leading-7 font-semibold text-secondary-800 simple-accordion__heading'>{item.title}</h3>
                                 <ChevronDownIcon className="w-8 h-8 ml-auto chevron" />
                             </label>
