95bea58ad6ee3220deef2645165549560698492e
IGuidoo <iguidoo@outlook.com>
2023-10-19 19:52:04 +0200

    d

--- src/components/SimpleAccordion.jsx
+++ src/components/SimpleAccordion.jsx
@@ -9,9 +9,9 @@ const Accordion = ({ data = [], cssClass, cssIndivClass }) => {
     return (
         <div className={`${cssClass}`} >
             {data.map((item, index) => (
-                <div>
-                    <input type="checkbox" name="detail-one" id={`detail-${index}`} className='simple-accordion__input' />
-                    <details open key={item.title} className={` simple-accordion text-base leading-6 font-medium text-secondary-700 md:text-lg md:leading-7 ${cssIndivClass}`}>
+                <div key={item.title}>
+                    <input type="checkbox" name={`detail-${index}`} id={`detail-${index}`} className='simple-accordion__input' />
+                    <details open  className={` simple-accordion text-base leading-6 font-medium text-secondary-700 md:text-lg md:leading-7 ${cssIndivClass}`}>
                         <summary className='p-8'>
                             <label for={`detail-${index}`} className='flex gap-6 simple-accordion__label'>
                                 <h3 className='text-xl leading-7 font-semibold text-secondary-800 simple-accordion__heading'>{item.title}</h3>
