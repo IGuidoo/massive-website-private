255a821585b18e581404a3f301a030595adeb2fd
IGuidoo <iguidoo@outlook.com>
2023-10-22 14:03:42 +0200

    Install Partytown, add lottie into it

--- src/components/animations/Animation.js
+++ src/components/animations/Animation.js
@@ -3,18 +3,16 @@
 // npm install --save @lottiefiles/react-lottie-player
 
 import React, { useEffect, useRef } from "react";
+// import { Partytown } from '@builder.io/partytown/react';
 import bodymoving_test from './bodymoving_test.json';
-// import animationData from '../assets/animations/test.json';
 import lottie from 'lottie-web';
 
 const AnimationRef = () => {
     const animatedElRef = useRef(null);
     const lottieAnimationRef = useRef(null);
 
-    // Play animation on mount
     useEffect(() => {
         if (animatedElRef.current) {
-            // Settings 
             const animation = lottie.loadAnimation({
                 container: animatedElRef.current,
                 renderer: "svg",
@@ -29,7 +27,7 @@ const AnimationRef = () => {
 
     return (
         <div>
-            <script defer type="module" src="/lottie.min.js" ></script>
+            <script defer type="text/partytown" src="/lottie.min.js" />
 
             {/* Canvas for Animation */}
             <div ref={animatedElRef} className="animatedName"></div>
